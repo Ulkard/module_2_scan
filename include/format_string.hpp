a8eca690e7d0205206b46c06b980cde3a80c7737
Dmitrii Obryvalin <grishnov82@yandex.com>
2025-06-06 12:11:38 +0300

    p.1-6 implement fixed_string & format_string

--- include/format_string.hpp
+++ include/format_string.hpp
@@ -1,96 +1,130 @@
 #pragma once
 
+#include <cstddef>
 #include <expected>
 
 #include "types.hpp"
 
 namespace stdx::details {
 
-// Шаблонный класс для хранения форматирующей строчки и ее особенностей
-// ваш код здесь
+template <template <typename> typename fixed_string>
 class format_string {
-    // ваш код здесь
-};
+    template <size_t Size>
+    using PlaceholderPoses = std::array<std::pair<size_t, size_t>, Size>;
 
-// Пользовательский литерал
-/*
-ваш код здесь
-ваш код здесь operator"" _fs()  сигнатуру также поменяйте
-{
-ваш код здесь
-}
-*/
+    // Функция для получения количества плейсхолдеров и проверки корректности формирующей строки
+    static consteval std::expected<size_t, parse_error> get_number_placeholders() {
+        constexpr size_t N = str.size();
+        if (!N)
+            return 0;
+        size_t placeholder_count = 0;
+        size_t pos = 0;
+        const size_t size = N - 1; // -1 для игнорирования нуль-терминатора
 
-// Функция для получения количества плейсхолдеров и проверки корректности формирующей строки
-// Функция закомментирована, так как еще не реализованы классы, которые она использует
-/*
-// Сделайте эту свободную функцию методом класса format_string
-template<fixed_string str>
-consteval std::expected<size_t, parse_error> get_number_placeholders() {
-    constexpr size_t N = str.size();
-    if (!N)
-        return 0;
-    size_t placeholder_count = 0;
-    size_t pos = 0;
-    const size_t size = N - 1; // -1 для игнорирования нуль-терминатора
+        while (pos < size) {
+            // Пропускаем все символы до '{'
+            if (str.data[pos] != '{') {
+                ++pos;
+                continue;
+            }
 
-    while (pos < size) {
-        // Пропускаем все символы до '{'
-        if (str.data[pos] != '{') {
-            ++pos;
-            continue;
-        }
-
-        // Проверяем незакрытый плейсхолдер
-        if (pos + 1 >= size) {
-            return std::unexpected(parse_error{"Unclosed last placeholder"});
-        }
-
-        // Начало плейсхолдера
-        ++placeholder_count;
-        ++pos;
-
-        // Проверка спецификатора формата
-        if (str.data[pos] == '%') {
-            ++pos;
-            if (pos >= size) {
+            // Проверяем незакрытый плейсхолдер
+            if (pos + 1 >= size) {
                 return std::unexpected(parse_error{"Unclosed last placeholder"});
             }
 
-            // Проверяем допустимые спецификаторы
-            const char spec = str.data[pos];
-            constexpr char valid_specs[] = {'d', 'u', 'f', 's'};
-            bool valid = false;
+            // Начало плейсхолдера
+            ++placeholder_count;
+            ++pos;
 
-            for (const char s : valid_specs) {
-                if (spec == s) {
-                    valid = true;
-                    break;
+            // Проверка спецификатора формата
+            if (str.data[pos] == '%') {
+                ++pos;
+                if (pos >= size) {
+                    return std::unexpected(parse_error{"Unclosed last placeholder"});
                 }
+
+                // Проверяем допустимые спецификаторы
+                const char spec = str.data[pos];
+                constexpr char valid_specs[] = {'d', 'u', 'f', 's'};
+                bool valid = false;
+
+                for (const char s : valid_specs) {
+                    if (spec == s) {
+                        valid = true;
+                        break;
+                    }
+                }
+
+                if (!valid) {
+                    return std::unexpected(parse_error{"Invalid specifier."});
+                }
+                ++pos;
             }
 
-            if (!valid) {
-                return std::unexpected(parse_error{"Invalid specifier."});
+            // Проверяем закрывающую скобку
+            if (pos >= size || str.data[pos] != '}') {
+                return std::unexpected(parse_error{"\'}\' hasn't been found in appropriate place"});
             }
             ++pos;
         }
 
-        // Проверяем закрывающую скобку
-        if (pos >= size || str.data[pos] != '}') {
-            return std::unexpected(parse_error{"\'}\' hasn't been found in appropriate place"});
-        }
-        ++pos;
+        return placeholder_count;
     }
 
-    return placeholder_count;
-}
-*/
+    // Функция для получения позиций плейсхолдеров
 
-// Функция для получения позиций плейсхолдеров
+    template <size_t PhSize>
+    static consteval PlaceholderPoses<PhSize> get_placeholder_positions() { 
+        constexpr size_t N = str.size();
+        PlaceholderPoses<PhSize> result;
+        if (!N)
+            return result;
+        size_t current_idx = 0;
+        size_t pos = 0;
+        const size_t size = N - 1; // -1 для игнорирования нуль-терминатора
 
-// ваш код здесь
-void get_placeholder_positions() {  // сигнатуру тоже нужно изменить
-    // ваш код здесь
+
+        while (pos < size) {
+            // Пропускаем все символы до '{'
+            if (str.data[pos] != '{') {
+                ++pos;
+                continue;
+            }
+        
+            // Начало плейсхолдера
+            size_t ph_begin = pos;
+            ++pos;
+            while (pos < size) {
+                if (str.data[pos] == '}') {
+                    result[current_idx] = {ph_begin, pos};
+                    ++current_idx;
+                }
+                ++pos;
+            }
+
+            ++pos;
+        }
+
+        static_assert(current_idx == PhSize - 1, "unexpected PhSize");
+
+        return result;
+    }
+
+    static fixed_string str;
+    static constexpr size_t number_placeholders = [result = get_number_placeholders()](){
+        return result.has_value() ? result.value() : 0;
+    }();
+    static constexpr PlaceholderPoses<number_placeholders> placeholder_positions = get_placeholder_positions();
+};
+
+// Пользовательский литерал
+
+template <fixed_string FixedString>
+constexpr auto operator"" _fs()  
+{
+    return FixedString;
 }
+
 
 } // namespace stdx::details
--- include/types.hpp
+++ include/types.hpp
@@ -1,26 +1,47 @@
 #pragma once
 
+#include <algorithm>
+#include <cstring>
+#include <cstddef>
+#include <iterator>
 namespace stdx::details {
 
 // Шаблонный класс, хранящий C-style строку фиксированной длины
-
-// ваш код здесь
+template <size_t ArrSize>
 struct fixed_string {
-    // ваш код здесь
+    char data[ArrSize];
+
+    constexpr fixed_string(const char other[ArrSize]) {
+        std::strcpy(data, other);
+    }
+
+    template <size_t OtherSize>
+    constexpr fixed_string(const char other[OtherSize]) requires (OtherSize <= ArrSize) {
+        std::strcpy(data, other);;
+    }
+
+    constexpr fixed_string(char* src_begin, char* src_end) requires (src_begin <= src_end) {
+        auto distance = src_end - src_begin;
+        std::copy(src_begin, src_end + 1, &data, &data + distance);
+    }
+
+    constexpr size_t size() {
+        return ArrSize;
+    }
 };
 
 // Шаблонный класс, хранящий fixed_string достаточной длины для хранения ошибки парсинга
-
-// ваш код здесь
-struct parse_error {};
+struct parse_error : public fixed_string<40> {};
 
 // Шаблонный класс для хранения результатов парсинга
-
 template <typename... Ts>
 struct scan_result {
-// ваш код здесь
-// измените реализацию
-    int i;
+    std::tuple<Ts...> values() {
+        return values_;
+    }
+
+private:
+    std::tuple<Ts...> values_;
 };
 
 } // namespace stdx::details
